9549d78d4de1b8f9569faee79934f47824d3eb13
JayYosh <133554802+JayYosh@users.noreply.github.com>
2025-12-02 11:53:25 -0600

    add springboot starter kit project

---
+++ aggregation-model/src/main/java/com/quicken/aggregation_model/AggregationModelApplication.java
@@ -0,0 +1,13 @@
+package com.quicken.aggregation_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregationModelApplication.class, args);
+	}
+
+}
