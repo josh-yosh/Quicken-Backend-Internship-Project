3cc43ac8211eca61a8b9203e6140377b60f5a1f5
JayYosh <133554802+JayYosh@users.noreply.github.com>
2025-12-04 18:10:15 -0600

    Create AggregationService interface. add SummaryVO for getAccountSumary

--- aggregation-model/src/main/java/com/quicken/aggregation_model/service/AggregationService.java
+++ aggregation-model/src/main/java/com/quicken/aggregation_model/service/AggregationService.java
@@ -1,5 +1,27 @@
 package com.quicken.aggregation_model.service;
 
+import java.sql.Date;
+import java.util.List;
+
+import com.quicken.aggregation_model.model.Account;
+import com.quicken.aggregation_model.vo.SummaryVO;
+
 public interface AggregationService {
-    
+    //pre: true
+    //part of post: if user has no Account, returns empty list
+    List<Account> getAllAccounts();
+
+    //pre: account id must exist
+    //post: description of Account
+    String getAccountDescription(long id);
+
+    //pre: account id must exist
+    //post: a SummaryVO with int values income, expenses, net.
+    SummaryVO getAccountSummary(long id);
+
+    //pre: account id must exist
+    //pre: startDate and endDate have to be valid dates
+    //post: a SummaryVO with int values income, expenses, net.
+    //post: a timeRange with no transactions will return a SummaryVO with 0's income, expenses, net values.
+    SummaryVO getAccountSummary(long id, Date startDate, Date endDate);
 }
---
+++ aggregation-model/src/main/java/com/quicken/aggregation_model/vo/SummaryVO.java
@@ -0,0 +1,5 @@
+package com.quicken.aggregation_model.vo;
+
+public class SummaryVO {
+
+}
